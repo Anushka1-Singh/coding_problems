1b2ece80846bc406aa8bc19ba5e679f222d235a0
Anushka Singh <9651anushka@gmail.com>
2026-03-18 22:54:52 +0530

    add

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,9 @@
 n=10 #number of terms
 n1=0
 n2=1
-print(n1,n2,end='')
+print(n1,n2,end=' ')
 for i in range(2,n):   # 8 iterations
     n3=n1+n2
-    print(n3,end='')
+    print(n3,end=' ')
     n1=n2
     n2=n3
