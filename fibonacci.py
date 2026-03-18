30f5184c8349d49e3787f23a365d00ca41a45356
Anushka Singh <9651anushka@gmail.com>
2026-03-18 23:10:06 +0530

    add

--- fibonacci.py
+++ fibonacci.py
@@ -7,3 +7,23 @@ for i in range(2,n):   # 8 iterations
     print(n3,end=' ')
     n1=n2
     n2=n3
+print(n)
+# while condition
+num=int(input('enter no. of terms : '))
+num1=0
+num2=1
+count=2
+if num<=0:
+    print('invalid input')
+elif num==1:
+    print(num1,num2,end=' ')
+else:
+    print(num1,num2,end=' ')
+    while count<num:
+        num3=num1+num2
+        print(num3,end=' ')
+        num1=num2
+        num2=num3
+
+        count+=1
+        
