fad9dc20a0462bdcd761678a70fb3959d4f0c546
Anushka Singh <9651anushka@gmail.com>
2026-03-19 21:38:10 +0530

    add

--- string_reverse.py
+++ string_reverse.py
@@ -27,11 +27,21 @@ for test in test_cases:
     result = reverse_string(test)
     print(f"Input: '{test}' -> Output: '{result}'")
 
-
+# reversed built in method which returns the reverse string
 s1 = 'rohit'
 reversed_s1 =reversed(s1)   #give you characters in reverse… but only when you ask for them one by one”
 print(reversed_s1)          # reversed_s1 : An iterator is an object that produces items one at a time, on demand, instead of storing them all at once.
 
 s2 = "rohit"
 for char in reversed(s2):
-    print(char, end='')
+    print(char, end='')     # end= After printing this, append this string instead of going to next line
+
+
+# while loop
+s3='rohit'
+reversed_s3=''
+i=len(s3)-1
+while i>=0:
+    reversed_s3 += s3[i]
+    i -= 1
+print(reversed_s3,'using while loop')
