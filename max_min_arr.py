52fed8d7294a79c9011c99c3cab45341066e5c4a
Anushka Singh <9651anushka@gmail.com>
2026-03-20 22:33:51 +0530

    add

--- max_min_arr.py
+++ max_min_arr.py
@@ -11,3 +11,15 @@ def min_max(arr):
 arr=[64,34,25,12,22,11,90]
 low,high=min_max(arr)
 print(f"min:{low},max:{high}")
+
+# using bubble sort algorithm
+def bubble_sort_min_max(arr):
+    n=len(arr)
+    for i in range(n):
+        for j in range(0,n-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+    return arr[0],arr[n-1]
+data=[64,34,25,12,22,11,90]
+low,high=bubble_sort_min_max(data)
+print(f'Min: {low}, Max:{high}')
