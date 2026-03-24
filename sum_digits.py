7dc5f4a365db51f2aaf64a4444f808756589db1b
Anushka Singh <9651anushka@gmail.com>
2026-03-24 21:17:43 +0530

    add

--- sum_digits.py
+++ sum_digits.py
@@ -1,3 +1,4 @@
+# while loop
 s=0
 n=int(input('enter the number : '))
 while n>0:
@@ -5,3 +6,19 @@ while n>0:
     s = s+m
     n=n//10
 print('the sum of digits is : ',s)
+
+
+# for loop with string
+N=input('enter the no : ')
+digit_sum=0
+for digit in N:
+    digit_sum = digit_sum + int(digit)
+print('sum of digits : ',digit_sum)
+
+# recursion based sum of digits
+def sum_digit(n):
+    if n==0:
+        return 0
+    return n%10 + sum_digit(n//10)
+n = 4538
+print('recursion based sum of digits',sum_digit(n))
