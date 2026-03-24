af3d00b224ad22bd7e6f7663f91b07f33bd7eda5
Anushka Singh <9651anushka@gmail.com>
2026-03-24 21:21:15 +0530

    added

--- sum_digits.py
+++ sum_digits.py
@@ -20,5 +20,17 @@ def sum_digit(n):
     if n==0:
         return 0
     return n%10 + sum_digit(n//10)
-n = 4538
+n = 456
 print('recursion based sum of digits',sum_digit(n))
+
+# sum_digit(456)
+# = 456 % 10 + sum_digit(456//10)
+# = 6 + sum_digit(45)
+# sum_digit(45)
+# = 45 % 10 + sum_digit(45//10)
+# = 5 + sum_digit(4)
+# sum_digit(4)
+# = 4 % 10 + sum_digit(4//10) 
+# = 4 + sum_digit(0)
+# = sum_digit(0)
+# = 0 -> base case reached
